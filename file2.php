3f39e2bb7ca8dcf325e6b97f42abe43e9493a6ff
plnklkv <polinakulakova04072@gmail.com>
2022-11-02 21:12:36 +0700

    Операторы в PHP

---
+++ file2.php
@@ -0,0 +1,25 @@
+<?php
+
+var_dump(!1);
+echo '<br>';
+var_dump(!0);
+echo '<br>';
+var_dump(!true);
+echo '<br>';
+var_dump(2 && 3);
+echo '<br>';
+var_dump(5 && 0);
+echo '<br>';
+var_dump(3 || 0);
+echo '<br>';
+var_dump(5 / 1);
+echo '<br>';
+var_dump(1 / 5);
+echo '<br>';
+var_dump(5 + '5string');
+echo '<br>';
+var_dump('5' == 5);
+echo '<br>';
+var_dump('05' == 5);
+echo '<br>';
+var_dump('05' == '5');
